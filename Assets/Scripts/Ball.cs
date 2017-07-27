37899ca2be0e37402cc28acb11a01c2bfd7003ce
Alice Robinson <valkrysa@gmail.com>
2017-07-26 23:07:39 -0700

    through lesson 192, ball and pins setup

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float launchSpeed;
+
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        Launch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Launch()
+    {
+        // rigidBody.AddForce(transform.forward * 100000.0f);
+        rigidBody.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+}
